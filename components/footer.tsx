2a2c6c57e5d57797264b1f6772b9aa7e5cca9bca
meganlabs <77144101+meganlabs@users.noreply.github.com>
2023-09-18 13:32:37 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -19,6 +19,10 @@ const Footer: NextPage = () => {
           <h2 className="app">Download UPI Link Offical App</h2>
         </Link>
       </p>
+      <p className="tpf">
+        <Link href={"http://bit.ly/3JMWNQc"} passHref>
+          <h2 className="app">Download UPI Link Offical App</h2>
+        </Link>
       <p className="tuhin">
          {" "}
         <a
