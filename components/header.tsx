2716dbfa0ba23306493791c1cb1c8fa4c539b96c
meganlabs <77144101+meganlabs@users.noreply.github.com>
2023-09-21 17:39:31 +0530

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from "next";
 import Link from "next/link";
 import * as gtag from "../analytic/gtag";
-import { useEffect } from "react"; 
 
 interface Props {
   title?: string;
