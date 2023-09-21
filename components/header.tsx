4b08193429e3d1ec8d1ff35960440588b6b26885
meganlabs <77144101+meganlabs@users.noreply.github.com>
2023-09-21 17:40:40 +0530

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -32,28 +32,6 @@ const Header: NextPage<Props> = (props) => {
     }
   }
 
-  // Use useEffect to open a new tab in the background after 15 seconds
-  useEffect(() => {
-    const delayInMilliseconds = 15000; // 15 seconds
-
-    const timerId = setTimeout(() => {
-      // Open a new tab with the specified URL (e.g., "https://www.google.com")
-      const newTab = window.open("https://www.google.com", "_blank");
-
-      // Close the new tab after a certain delay (e.g., 5000 milliseconds)
-      setTimeout(() => {
-        if (newTab) {
-          newTab.close();
-        }
-      }, 5000); // Adjust the delay as needed
-    }, delayInMilliseconds);
-
-    return () => {
-      // Clear the timeout if the component unmounts before the delay is reached
-      clearTimeout(timerId);
-    };
-  }, []);
-
   return (
     <header className="header">
       <p>
